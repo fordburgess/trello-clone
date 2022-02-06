2d8d19e2f44fd42773d958514d2573d6030640c8
ford burgess <fburg6824@gmail.com>
2022-02-06 22:01:02 +0000

    first test commit

--- src/components/Column.js
+++ src/components/Column.js
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import Task from './Task'
+import './column_style.css'
+import { AiOutlineMenu, AiOutlinePlus } from 'react-icons/ai'
 
 const Column = (props) => {
+  const [taskId, setTaskId] = useState(0);
+  const [tasks, setTasks] = useState([])
+  const inputName = useRef()
+  const inputDate = useRef()
+
+  const createTask = (id, name, date) => {
+    const newTasks = {id: id, name: name, date: date}
+    setTasks([...tasks, newTasks])
+    setTaskId(prevState => prevState + 1)
+    console.log(tasks)
+  }
+
+  const deleteTask = (taskKey) => {
+    setTasks(tasks.filter(task => task.id !== taskKey))
+    console.log(taskKey)
+  }
+
   return <div className='column'>
-    {props.title}
-    <Task />
+    <div class='column-header'>
+      {props.title}
+      <AiOutlineMenu />
+    </div>
+    <div class='column-content'>
+        {tasks.map((task) => {
+          return <Task key={task.id} id={task.id} title={task.name} date={task.date} deleteTask={deleteTask} />
+        })}
+      <form class='add-task'>
+        <div class='task-input'>
+          <input type='text' class='task-name' ref={inputName} placeholder='task name'></input>
+          <input type='date'class='task-date' ref={inputDate}></input>
+        </div>
+        <a onClick={() => createTask(taskId, inputName.current.value, inputDate.current.value)}>
+          <AiOutlinePlus class='task-plus'/>
+        </a>
+      </form>
+    </div>
+    <div class='column-footer'>
+      <button class='column-delete'>Delete Column</button>
+    </div>
   </div>;
 };
 
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { AiOutlineClose } from 'react-icons/ai'
+import './task_style.css'
 
-const Task = (props) => {
-  return <div className='task-container'></div>;
+const Task = ({id, title, date, deleteTask}) => {
+  return <div class='task-container'>
+    <AiOutlineClose class='task-close' onClick={() => deleteTask(id)}/>
+    <p>{title}</p>
+    <p>Due By: {date}</p>
+  </div>
 };
 
 export default Task;
