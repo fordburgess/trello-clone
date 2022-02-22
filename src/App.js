389a53ad16cfb486996b772949c81bbf150800c2
strazan <sigge@labor.cool>
2022-02-22 23:30:03 +0100

    [fix] change component={Login} to element={<Login/>}

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import './App.css'
 import Column from './components/Column'
 import Header from './components/Header'
-export const HeightContext = React.createContext();
-
+export const HeightContext = React.createContext()
 
 function App() {
   const [name, setName] = useState('')
@@ -11,66 +10,72 @@ function App() {
   const [columns, setColumns] = useState([])
   // const uniqueHeight = { height : "auto" }
   const createColumn = (id, title) => {
-    const newColumn = { id: id, title: title }
+    const newColumn = {id: id, title: title}
     setColumns([...columns, newColumn])
     setColumnId(prevState => prevState + 1)
     console.log(columnId)
     setName('')
   }
-  const deleteColumn = (columnId) => {
+  const deleteColumn = columnId => {
     setColumns(columns.filter(column => column.columnId !== columnId))
   }
   //column fetch
   const endpoint = 'https://trello-clone1.hasura.app/v1/graphql'
   const headers = {
     'x-hasura-admin-secret': '6sgmC96b5CkHOai3cwJbfzMLeTqJebUemTdLayvc6rwpTRc6vlDWnuyP4qvfLk24',
-    'content-type' : 'application/json'
+    'content-type': 'application/json',
   }
   const graphqlQuery = {
-    "operationName" : 'fetchColumns',
-    "query" : `query fetchColumns {
+    operationName: 'fetchColumns',
+    query: `query fetchColumns {
       tasks {
         status
       }
       }`,
   }
   const options = {
-    "method": "POST",
-    "headers": headers,
-    "body": JSON.stringify(graphqlQuery)
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify(graphqlQuery),
   }
   useEffect(() => {
-    fetch(endpoint, options).then(res => res.json()).then(data => {
-      const tasks = [...data.data.tasks]
-      const statuses = [];
-      tasks.forEach(task => {
-        if (!statuses.includes(task.status)) {
-          statuses.push(task.status)
-        }
-      });
-      statuses.forEach(status => {
-        // console.log(status)
-        createColumn(columnId, status)
-        setColumnId(prevState => prevState + 1);
+    fetch(endpoint, options)
+      .then(res => res.json())
+      .then(data => {
+        const tasks = data.data.tasks
+        const statuses = []
+        tasks.forEach(task => {
+          if (!statuses.includes(task.status)) {
+            console.log(task)
+            statuses.push(task.status)
+          }
+        })
+        console.log(statuses)
+        statuses.forEach(status => {
+          createColumn(columnId, status)
+          setColumnId(prevState => prevState + 1)
+        })
       })
-    })
-  }, []);
+  }, [])
+
   return (
     <div className="App">
       <Header />
-      <div class='sub-header'>
-        <input type='text' class='add-input' placeholder='column name' onChange={(e) => setName(e.target.value)}/>
-        <button class='add-button' onClick={() => createColumn(columnId, name)}>Add Column</button>
+      <div class="sub-header">
+        <input type="text" class="add-input" placeholder="column name" onChange={e => setName(e.target.value)} />
+        <button class="add-button" onClick={() => createColumn(columnId, name)}>
+          Add Column
+        </button>
       </div>
       {/* <HeightContext.Provider value={uniqueHeight}> */}
-        <div className='container'>
+      <div className="container">
         {columns.map(column => {
-          return <Column key={column.columnId} id={column.columndId} title={column.title} deleteColumn={deleteColumn}/>
+          return <Column key={column.columnId} id={column.columndId} title={column.title} deleteColumn={deleteColumn} />
         })}
-        </div>
+      </div>
       {/* </HeightContext.Provider> */}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
