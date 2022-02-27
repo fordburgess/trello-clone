a31e438f04bd3ce1a963f9ea14414f25c274e8f7
ford burgess <fburg6824@gmail.com>
2022-02-27 17:58:13 +0000

    login page sort of works

---
+++ src/components/Auth0.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import '../Login.css'
+
+const LoginButton = () => {
+  const { loginWithRedirect } = useAuth0();
+
+  return <button class='login-button' onClick={() => loginWithRedirect()}>Log In</button>;
+};
+
+const LogoutButton = () => {
+  const { logout } = useAuth0();
+
+  return (
+    <button onClick={() => logout({ returnTo: window.location.origin })}>
+      Log Out
+    </button>
+  );
+};
+
+export { LoginButton, LogoutButton };
--- src/login.js
+++ src/login.js
@@ -1,8 +1,17 @@
 import React from 'react'
-// import LoginButton from './LoginButton';
+import { LoginButton } from './components/Auth0'
+// import { LogoutButton } from './components/Auth0'
+import './Login.css'
 
 const Login = () => {
-  return <div class="login-container">Hello</div>
+  return <div class="poop">
+    <div class='info'>
+      <div class='content'>
+        <h1>Organize Your Life</h1>
+        <LoginButton />
+      </div>
+    </div>
+  </div>
 }
 
 export default Login
